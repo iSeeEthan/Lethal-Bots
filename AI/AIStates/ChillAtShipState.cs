iSeeEthan/Lethal-Bots

commit 5dbf475775762b6c156a120ca0439f89b1329037

Cleaned up some code
- Made bots skip the wait time they normaly have when they return to the ship if they are at the company building. This should make them more proactive when selling.
- Changed HasGrabbableObjectInInventory, the filter version, and TryFindItemInInventory to now check for null objects internally. I don't know why I didn't make them do it before.....All parts of code that use these functions have been updated accordingly!
- Made a minor optimization to ChillAtShipState

AI/AIStates/ChillAtShipState.cs
@@ -61,6 +61,7 @@ namespace LethalBots.AI.AIStates
             }
 
             // If we are holding anything we should drop it
+            bool canInverseTeleport = true;
             if (npcController.Npc.isInHangarShipRoom)
             {
                 // Bot drop item
@@ -68,19 +69,14 @@ namespace LethalBots.AI.AIStates
                 if (!ai.AreHandsFree())
                 {
                     ai.DropItem();
+                    canInverseTeleport = false;
                 }
                 // If we still have stuff in our inventory,
                 // we should swap to it and drop it!
-                else if (ai.HasSomethingInInventory())
+                else if (ai.HasGrabbableObjectInInventory(FindObjectToDrop, out int objectSlot))
                 {
-                    for (int i = 0; i < npcController.Npc.ItemSlots.Length; i++)
-                    {
-                        if (npcController.Npc.ItemSlots[i] != null)
-                        {
-                            ai.SwitchItemSlotsAndSync(i);
-                            break;
-                        }
-                    }
+                    ai.SwitchItemSlotsAndSync(objectSlot);
+                    canInverseTeleport = false;
                 }
                 else if (missionController == npcController.Npc)
                 {
@@ -92,22 +88,24 @@ namespace LethalBots.AI.AIStates
                     if (missionController == null || !missionController.isPlayerControlled || missionController.isPlayerDead)
                     {
                         LethalBotManager.Instance.MissionControlPlayer = npcController.Npc;
+                        canInverseTeleport = false;
                     }
                 }
             }
 
             // Is the inverse teleporter on, we should use it!
             if (LethalBotManager.IsInverseTeleporterActive 
-                && !ai.HasSomethingInInventory())
+                && canInverseTeleport)
             {
                 ai.State = new UseInverseTeleporterState(this);
                 return;
             }
 
-            if (chillAtShipTimer > Const.TIMER_CHILL_AT_SHIP)
+            bool areWeAtTheCompany = LethalBotManager.AreWeAtTheCompanyBuilding();
+            if (chillAtShipTimer > Const.TIMER_CHILL_AT_SHIP || areWeAtTheCompany)
             {
                 // If we are at the company building, we should sell!
-                if (LethalBotManager.AreWeAtTheCompanyBuilding())
+                if (areWeAtTheCompany)
                 {
                     if(ai.LookingForObjectsToSell(true) != null || LethalBotManager.AreThereItemsOnDesk())
                     {
@@ -274,6 +272,19 @@ namespace LethalBots.AI.AIStates
                 return;
             }
             base.OnSignalTranslatorMessageReceived(message);
+        }
+
+        /// <summary>
+        /// Simple function that checks if the give <paramref name="item"/> is null or not
+        /// </summary>
+        /// <remarks>
+        /// This was designed for use in <see cref="LethalBotAI.HasGrabbableObjectInInventory(System.Func{GrabbableObject, bool}, out int)"/> calls.
+        /// </remarks>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private static bool FindObjectToDrop(GrabbableObject item)
+        {
+            return true; // Found an item, great, we want to drop it!
         }
 
         /// <summary>

AI/AIStates/SellScrapState.cs
@@ -203,13 +203,12 @@ namespace LethalBots.AI.AIStates
         /// Helper function to check if the given <paramref name="item"/> is a object we can sell!
         /// </summary>
         /// <remarks>
-        /// This was designed for use in <see cref="LethalBotAI.HasGrabbableObjectInInventory(Func{GrabbableObject?, bool}, out int)"/> calls.
+        /// This was designed for use in <see cref="LethalBotAI.HasGrabbableObjectInInventory(Func{GrabbableObject, bool}, out int)"/> calls.
         /// </remarks>
         /// <param name="item"></param>
         /// <returns></returns>
-        private bool FindNextSellableObject(GrabbableObject? item)
+        private bool FindNextSellableObject(GrabbableObject item)
         {
-            if (item == null) return false;
             return ai.IsGrabbableObjectSellable(item, true, true);
         }
 

AI/AIStates/UseKeyOnLockedDoorState.cs
@@ -295,11 +295,11 @@ namespace LethalBots.AI.AIStates
         /// Helper function to check if the given <paramref name="item"/> is a key or lockpicker!
         /// </summary>
         /// <remarks>
-        /// This was designed for use in <see cref="LethalBotAI.TryFindItemInInventory(System.Func{GrabbableObject?, bool}, System.Func{GrabbableObject, GrabbableObject?, bool}, out int)"/> calls.
+        /// This was designed for use in <see cref="LethalBotAI.TryFindItemInInventory(System.Func{GrabbableObject, bool}, System.Func{GrabbableObject, GrabbableObject, bool}, out int)"/> calls.
         /// </remarks>
         /// <param name="item"></param>
         /// <returns></returns>
-        private static bool IsKeyItem(GrabbableObject? item)
+        private static bool IsKeyItem(GrabbableObject item)
         {
             return item is KeyItem || item is LockPicker;
         }
@@ -308,7 +308,7 @@ namespace LethalBots.AI.AIStates
         /// Helper function to check if the <paramref name="canidate"/> is better than our <paramref name="currentBest"/>!
         /// </summary>
         /// <remarks>
-        /// This was designed for use in <see cref="LethalBotAI.TryFindItemInInventory(System.Func{GrabbableObject?, bool}, System.Func{GrabbableObject, GrabbableObject?, bool}, out int)"/> calls.
+        /// This was designed for use in <see cref="LethalBotAI.TryFindItemInInventory(System.Func{GrabbableObject, bool}, System.Func{GrabbableObject, GrabbableObject, bool}, out int)"/> calls.
         /// </remarks>
         /// <param name="currentBest">The best grabbable object we have found so far.</param>
         /// <param name="canidate">The next object that past the filter earlier in the function.</param>
@@ -316,11 +316,7 @@ namespace LethalBots.AI.AIStates
         private static bool IsBetterKey(GrabbableObject currentBest, GrabbableObject? canidate)
         {
             // We prefer the key over the lockpick if possible!
-            if (canidate == null)
-            {
-                return false;
-            }
-            else if (currentBest is not KeyItem && canidate is KeyItem)
+            if (currentBest is not KeyItem && canidate is KeyItem)
             {
                 return true;
             }

AI/AIStates/UseTZPInhalantState.cs
@@ -145,14 +145,13 @@ namespace LethalBots.AI.AIStates
         /// Helper function to check if the given <paramref name="item"/> is a usable <see cref="TetraChemicalItem"/>!
         /// </summary>
         /// <remarks>
-        /// This was designed for use in <see cref="LethalBotAI.HasGrabbableObjectInInventory(Func{GrabbableObject?, bool}, out int)"/> calls.
+        /// This was designed for use in <see cref="LethalBotAI.HasGrabbableObjectInInventory(Func{GrabbableObject, bool}, out int)"/> calls.
         /// </remarks>
         /// <param name="item"></param>
         /// <returns></returns>
-        private bool IsUsableTZPItem(GrabbableObject? item)
+        private static bool IsUsableTZPItem(GrabbableObject item)
         {
-            if (item != null
-                && item is TetraChemicalItem tempTZP
+            if (item is TetraChemicalItem tempTZP
                 && !tempTZP.itemUsedUp)
             {
                 return true;

AI/LethalBotAI.cs
@@ -3651,11 +3651,11 @@ namespace LethalBots.AI
         /// <param name="objectPredicate">The function to inspect the object in the inventory!</param>
         /// <param name="objectSlot">The slot of where the object was found at! Is set to -1 if item was not found!</param>
         /// <returns>true: the bot has the object in its inventory, false: the bot doesn't have the given object in its inventory</returns>
-        public bool HasGrabbableObjectInInventory(Func<GrabbableObject?, bool> objectPredicate, out int objectSlot)
+        public bool HasGrabbableObjectInInventory(Func<GrabbableObject, bool> objectPredicate, out int objectSlot)
         {
             // Check if the lethalBot is holding the object
             objectSlot = -1;
-            if (objectPredicate(HeldItem))
+            if (HeldItem != null && objectPredicate(HeldItem))
             {
                 objectSlot = NpcController.Npc.currentItemSlot;
                 return true;
@@ -3665,7 +3665,7 @@ namespace LethalBots.AI
             int index = 0;
             foreach (var item in NpcController.Npc.ItemSlots)
             {
-                if (objectPredicate(item))
+                if (item != null && objectPredicate(item))
                 {
                     objectSlot = index;
                     return true;
@@ -3686,7 +3686,7 @@ namespace LethalBots.AI
         /// <param name="isBetter">The function to determine if the found object is better than the current best one! First parameter is the current best item, second parameter is the new candidate item!</param>
         /// <param name="objectSlot">The slot of where the object was found at! Is set to -1 if item was not found!</param>
         /// <returns></returns>
-        public bool TryFindItemInInventory(Func<GrabbableObject?, bool> filter, Func<GrabbableObject, GrabbableObject?, bool> isBetter, out int objectSlot)
+        public bool TryFindItemInInventory(Func<GrabbableObject, bool> filter, Func<GrabbableObject, GrabbableObject, bool> isBetter, out int objectSlot)
         {
             // Unlike HasGrabbableObjectInInventory, we can't early out if the bot's held item matches the filter
             objectSlot = -1;
@@ -3696,7 +3696,7 @@ namespace LethalBots.AI
             int index = 0;
             foreach (var item in NpcController.Npc.ItemSlots)
             {
-                if (filter(item))
+                if (item != null && filter(item))
                 {
                     if (bestItem == null || isBetter(bestItem, item))
                     {

commit a2c8c457bf5a665deb0a2cf0e42cf75ee5f3bf11

Fixed a bug caused by 5dbf4757 where bots wouldn't properly enter the ChillAtShip state when at the company building

AI/AIStates/ChillAtShipState.cs
@@ -102,7 +102,8 @@ namespace LethalBots.AI.AIStates
             }
 
             bool areWeAtTheCompany = LethalBotManager.AreWeAtTheCompanyBuilding();
-            if (chillAtShipTimer > Const.TIMER_CHILL_AT_SHIP || areWeAtTheCompany)
+            float waitAtShipTime = areWeAtTheCompany ? 2f : Const.TIMER_CHILL_AT_SHIP;
+            if (chillAtShipTimer > waitAtShipTime)
             {
                 // If we are at the company building, we should sell!
                 if (areWeAtTheCompany)